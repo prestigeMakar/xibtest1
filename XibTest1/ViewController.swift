c3177db2fefbf8ee7780f3565f35aafd0004262d
Artem Makarenko <prestigemakar@gmail.com>
2022-10-31 17:16:20 +0300

    ViewController coder refactor

--- XibTest1/ViewController.swift
+++ XibTest1/ViewController.swift
@@ -1,17 +1,11 @@
-//
-//  ViewController.swift
-//  XibTest1
-//
-//  Created by Артем Макаренко on 31.10.2022.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
